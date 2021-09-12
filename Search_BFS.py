52d0d05370a5b9fd0d1dbc895171c486f000ba57
Anshumn Dey Kirty <anshuman_k@Anshumans-MacBook-Pro.local>
2021-09-12 10:40:59 -0700

    UCS Tested

--- Search_BFS.py
+++ Search_BFS.py
@@ -7,7 +7,8 @@ Code Structure:
 # Libraries
 import sys
 import numpy as np
-import heapq as hq
+from queue import PriorityQueue
+
 
 def convert_str_tuple_int(str_tuple):
     """
@@ -18,6 +19,7 @@ def convert_str_tuple_int(str_tuple):
         Tuple with int values.
     """
     return tuple(map(lambda x: int(x), str_tuple))
+
 
 def fetch_next_step(coordinate: tuple, step: int):
     """
@@ -33,41 +35,42 @@ def fetch_next_step(coordinate: tuple, step: int):
     y = int(coordinate[1])
     z = int(coordinate[2])
     if step == 1:
-        return x+1, y, z
+        return x + 1, y, z
     if step == 2:
-        return x-1, y, z
+        return x - 1, y, z
     if step == 3:
-        return x, y+1, z
+        return x, y + 1, z
     if step == 4:
-        return x, y-1, z
+        return x, y - 1, z
     if step == 5:
-        return x, y, z+1
+        return x, y, z + 1
     if step == 6:
-        return x, y, z-1
+        return x, y, z - 1
     if step == 7:
-        return x+1, y+1, z
+        return x + 1, y + 1, z
     if step == 8:
-        return x+1, y-1, z
+        return x + 1, y - 1, z
     if step == 9:
-        return x-1, y+1, z
+        return x - 1, y + 1, z
     if step == 10:
-        return x-1, y-1, z
+        return x - 1, y - 1, z
     if step == 11:
-        return x+1, y, z+1
+        return x + 1, y, z + 1
     if step == 12:
-        return x+1, y, z-1
+        return x + 1, y, z - 1
     if step == 13:
-        return x-1, y, z+1
+        return x - 1, y, z + 1
     if step == 14:
-        return x-1, y, z-1
+        return x - 1, y, z - 1
     if step == 15:
-        return x, y+1, z+1
+        return x, y + 1, z + 1
     if step == 16:
-        return x, y+1, z-1
+        return x, y + 1, z - 1
     if step == 17:
-        return x, y-1, z+1
+        return x, y - 1, z + 1
     if step == 18:
-        return x, y-1, z-1
+        return x, y - 1, z - 1
+
 
 def bfs_path_search(graph: dict, start_point: tuple, end_point: tuple):
     """
@@ -88,12 +91,12 @@ def bfs_path_search(graph: dict, start_point: tuple, end_point: tuple):
     for key in graph.keys():
         visited[key] = 0
     tracker = [[start_point]]
-
     while len(tracker) > 0:
         temp = tracker[0]
         tracker = tracker[1:]
         on = temp[-1]
         if visited[on] == 0:
+            visited[on] = 1
             neighbours = graph[on]
             for neighbour in neighbours:
                 short_path = list(temp)
@@ -101,73 +104,98 @@ def bfs_path_search(graph: dict, start_point: tuple, end_point: tuple):
                 tracker.append(short_path)
                 if neighbour == end_point:
                     return short_path
-            visited[on] = 1
     textfile = open(output_file, "w")
     textfile.write("FAIL")
     sys.exit()
 
+
+def get_distance(t1: tuple, t2: tuple):
+    a = np.array(t1)
+    b = np.array(t2)
+    dist = np.linalg.norm(a - b)
+    if dist == 1:
+        return 10
+    else:
+        return 14
+
+
 def graph_with_distance(graph: dict):
-    def get_distance(t1: tuple, t2: tuple):
-        a = np.array(t1)
-        b = np.array(t2)
-        dist = np.linalg.norm(a-b)
-        if dist == 1:
-            return 10
-        else:
-            return 14
     dist_graph = {}
     for key in graph.keys():
         path = {}
         for i in range(len(graph[key])):
-            path.update({graph[key][i]: get_distance(key, 
-                                                     graph[key][i])})
+            path.update({graph[key][i]: get_distance(key, graph[key][i])})
         dist_graph.update({key: path})
     return dist_graph
 
-def ucs_path_search(graph:dict, start_point: tuple, end_point: tuple):
-    graph = graph_with_distance(graph)
+
+def ucs_path_search(graph: dict, start_point: tuple, end_point: tuple):
     visited = {}
     for key in graph.keys():
         visited[key] = 0
-    tracker = {start_point: 0}
-    while len(tracker):
-        on = min(tracker, key=tracker.get)
-        value = tracker.pop(on)
-        temp = {on: value}
+    tracker = PriorityQueue()
+    tracker.put((0, (start_point), [start_point]))
+    while not tracker.empty():
+        cost, on, short_path = tracker.get()
         if visited[on] == 0:
-            neighbours = graph[on]
-            for neighbour in neighbours.keys():
-                #path = list(temp)
-                temp.update({neighbour: graph[on][neighbour]})
-                #print(temp)
-                tracker.update(temp)
-                print(tracker)
             visited[on] = 1
-                #if neighbour == end_point:
-                #    return short_path, 
-        
-
-def write_output(path_length: int, nodes_num: int, short_path: list):
+            neighbours = graph[on]
+            for neighbour in neighbours:
+                # if visited[neighbour] == 0:
+                total_cost = cost + get_distance(on, neighbour)
+                tracker.put((total_cost, neighbour, short_path + [neighbour]))
+                if neighbour == end_point:
+                    return (total_cost, short_path + [neighbour])
     textfile = open(output_file, "w")
-    textfile.write(str(path_length) + "\n")
-    textfile.write(str(nodes_num) + "\n")
-    for i in range (len(short_path)):
-        if i == 0:
-            textfile.write(" ".join(map(str, short_path[i]))+" 0\n")
-        elif i == len(short_path)-1:
-            textfile.write(" ".join(map(str, short_path[i]))+" 1")
-        else:
-            textfile.write(" ".join(map(str, short_path[i]))+" 1\n")
-    
+    textfile.write("FAIL")
+    sys.exit()
+
+
+def write_output(path_length: int, nodes_num: int, short_path: list, search_type: str):
+    if search_type == "BFS":
+        textfile = open(output_file, "w")
+        textfile.write(str(path_length) + "\n")
+        textfile.write(str(nodes_num) + "\n")
+        for i in range(len(short_path)):
+            if i == 0:
+                textfile.write(" ".join(map(str, short_path[i])) + " 0\n")
+            elif i == len(short_path) - 1:
+                textfile.write(" ".join(map(str, short_path[i])) + " 1")
+            else:
+                textfile.write(" ".join(map(str, short_path[i])) + " 1\n")
+    elif search_type == "UCS":
+        textfile = open(output_file, "w")
+        textfile.write(str(path_length) + "\n")
+        textfile.write(str(nodes_num) + "\n")
+        for i in range(len(short_path)):
+            if i == 0:
+                textfile.write(" ".join(map(str, short_path[i])) + " 0\n")
+                temp = short_path[i]
+            elif i == len(short_path) - 1:
+                textfile.write(
+                    " ".join(map(str, short_path[i]))
+                    + " "
+                    + str(get_distance(short_path[i], temp))
+                )
+            else:
+                textfile.write(
+                    " ".join(map(str, short_path[i]))
+                    + " "
+                    + str(get_distance(short_path[i], temp))
+                    + "\n"
+                )
+                temp = short_path[i]
+
+
 # Main Code
 # Reading Input Text File.
 lines = []
-input_file = "../Input2.txt"
+input_file = "../Input6.txt"
 output_file = "../Output.txt"
 with open(input_file) as f:
     lines = f.readlines()
 # Strip the \n read from the text file.
-lines = list(map(lambda x:x.strip(),lines))
+lines = list(map(lambda x: x.strip(), lines))
 
 # Assigning the values to variables from the text file.
 search_type = lines[0]
@@ -182,45 +210,27 @@ graph = {}
 # Loop to form the graph.
 for i in range(int(lines[4])):
     paths = []
-    for j in range(len(lines[i+5].split())-3):
-        next_step = fetch_next_step(tuple(lines[i+5].split()[:3]), 
-                                    int(lines[i+5].split()[j+3]))
+    for j in range(len(lines[i + 5].split()) - 3):
+        next_step = fetch_next_step(
+            tuple(lines[i + 5].split()[:3]), int(lines[i + 5].split()[j + 3])
+        )
         paths.append(next_step)
-        tuple_int = convert_str_tuple_int(tuple(lines[i+5].split()[:3]))
+        tuple_int = convert_str_tuple_int(tuple(lines[i + 5].split()[:3]))
     graph.update({tuple_int: paths})
-#print(graph)
+    # print(graph)
 
 # Edge Case
-    if end_point == start_point:
-        ls = []
-        ls.append(start_point)
-        write_output(0, 1, ls)
-        sys.exit()
-        
+if end_point == start_point:
+    ls = []
+    ls.append(start_point)
+    write_output(0, 1, ls)
+    sys.exit()
+
 if search_type == "BFS":
     short_path = bfs_path_search(graph, start_point, end_point)
-    write_output(len(short_path)-1, len(short_path), short_path)
+    write_output(len(short_path) - 1, len(short_path), short_path, search_type)
 elif search_type == "UCS":
-    short_path = ucs_path_search(graph, start_point,end_point)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    cost, short_path = ucs_path_search(graph, start_point, end_point)
+    write_output(cost, len(short_path), short_path, search_type)
+elif search_type == "A*":
+    cost, short_path = astar()
